0cd04d10985785d88dc4facc02ea4b0a786bd7eb
Ksumki <113994876+Ksumki@users.noreply.github.com>
2023-04-15 00:41:31 +0200

    Initial commit

---
+++ include/SortingAlgorithms.h
@@ -0,0 +1,12 @@
+// Header file for all the three sorting algorithms
+// it contains function prototypes of the sorting algorithms
+
+
+#ifndef SORTINGALGORITHMS_H
+#define SORTINGALGORITHMS_H
+
+
+
+
+
+#endif /* SORTINGALGORITHMS_H */
---
+++ src/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "SortingAlgorithms.h"
+
+
+int main ()
+{
+    
+}
