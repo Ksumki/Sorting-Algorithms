5ebe91db4d23aa3b710573b2f770c122c00432f8
Ksumki <113994876+Ksumki@users.noreply.github.com>
2023-04-15 15:25:15 +0200

    Create sorting.h

---
+++ include/sorting.h
@@ -0,0 +1,13 @@
+// This the header that we created for task 2
+// It includes the same function prototypes from Algorithms.h
+// The differnce between is that this header is from counter operation
+
+#ifndef SORTING_H
+#define SORTING_H
+
+
+
+
+
+
+#endif /* SORTING_H */
