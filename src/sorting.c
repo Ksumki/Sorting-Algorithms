b8f31095fbc78b99c196073e13e76478b34da6ae
MalaskaDeividas <126015993+MalaskaDeividas@users.noreply.github.com>
2023-04-18 10:58:11 +0200

    Update sorting.c
    La in selection sort //deivi

--- src/sorting.c
+++ src/sorting.c
@@ -1,6 +1,35 @@
 #include "sorting.h"
 #include "algorithms.h"
 
+
+void SelectionSort(unsigned int *arr, int n){
+
+    int i, j;
+    int min,temp;
+
+    for(i=0; i<n-1; i++){
+
+        min = i;
+
+        for(j=i+1; j<n; j++){
+
+            if(arr[j]<arr[min]){
+
+                min = j;
+
+            }
+        }
+
+        if(min != i){
+
+        temp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = temp;
+
+        }
+    }
+}
+
 void InsertSortOp(unsigned int arr[], int n, int* op)
 {
     for (int i = 0; i < n - 1; i++)
