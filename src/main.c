eec18bec11fb120465aadbaaf1135341f1271f39
MalaskaDeividas <126015993+MalaskaDeividas@users.noreply.github.com>
2023-04-19 22:04:36 +0200

    Update main.c
    la in printf meny för main//deivi

--- src/main.c
+++ src/main.c
@@ -4,10 +4,25 @@
 
 int main()
 {
-    int x = 53;
+    /*int x = 53;
     int* lst = mix4_array(x);
 
     for (int i = 0 ; i < x ; i++) {
         printf(" %d " , lst[i]);
-    }
+    }*/
+    
+    printf("\nAlgorithm: ");
+    printf("\nInput: ");
+    printf("\nSize n  ");
+    printf("\n-----------------------");
+    printf("\n256           ");
+    printf("\n512           ");
+    printf("\n1024          ");
+    printf("\n2048          ");
+    printf("\n4096          ");
+    printf("\n8192          ");
+    printf("\n32768         ");
+    
+    
+    
 }
